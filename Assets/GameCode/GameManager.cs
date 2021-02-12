87308ceb1d586ca62e609edda591ba1d1c203a60
kyleblazer <kyleblazer@gmail.com>
2021-02-11 21:57:58 -0800

    Initial commit

---
+++ Assets/GameCode/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public string filepath = "file.txt";
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeFile();
+        EditFile("Game has started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            EditFile("Spacebar has been pressed");
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        EditFile("Game has ended");
+    }
+    private void MakeFile()
+    {
+        if (!File.Exists(filepath))
+        {
+            File.Create(filepath).Close();
+        }
+    }
+    
+    public void EditFile(string stringMessage)
+    {
+        using (StreamWriter writer = new StreamWriter(filepath, true))
+        {
+            writer.Write(DateTime.Now);
+            writer.WriteLine(": "+stringMessage);
+        }
+    }
+
+}
